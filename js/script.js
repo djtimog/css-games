47f68243fd70926583a1248578707ee4d001d465
djtimog <ogunleyetimilehin14@gmail.com>
2024-07-19 12:50:38 -0700

    added css to javascript for some animation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
 let pageDiv = document.getElementById("container");
 let defaultPage = true;
+let game = 'flexbox';
+
 const updateImg = () => {
   let flexBoxStage = document.getElementById("flexbox-stage");
   let flexboxStageImg = document.getElementById("flexbox-stage-img");
-  flexboxStageImg.innerHTML = `<img src="img/flexbox-level-${flexBoxStage.value}.png" alt="Level ${flexBoxStage.value} Image">`;
+  flexboxStageImg.innerHTML = `<img src="img/${game}-level-${flexBoxStage.value}.png" alt="Level ${flexBoxStage.value} Image">`;
 };
+
 const challengeForFlexBox = () => {
+  document.getElementById('froggyflex').classList.add('btnnt');
+  document.getElementById('gardengrid').classList.remove('btnnt');
+  document.getElementById('container').classList.remove('container2');
   defaultPage = true;
+  game = 'flexbox';
   page();
-};
+}
+
 const challengeForGrid = () => {
+  document.getElementById('froggyflex').classList.remove('btnnt');
+  document.getElementById('gardengrid').classList.add('btnnt');
+  document.getElementById('container').classList.add('container2');
   defaultPage = false;
+  game = 'gridgraden';
   page();
-};
+}
+
 const imageLevel = (levels) => {
   let flexBoxStage = document.getElementById("flexbox-stage");
   for (let i = 0; i < levels.length; i++) {
@@ -20,12 +33,14 @@ const imageLevel = (levels) => {
   }
   updateImg();
 };
+
 const page = () => {
   if (defaultPage == true) {
     const levels = [
       1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
       22, 23, 24,
     ];
+
     pageDiv.innerHTML = `
   <div class="flex-box-froggy">
           <h1>Froggy Flexbox</h1>
@@ -36,12 +51,17 @@ const page = () => {
       </div>
     <div id="flexbox-stage-img"></div>
   </div>`;
-    document.getElementById("flexbox-stage").innerHTML = `<option value="Completed">Level Completed</option>`;
+    document.getElementById(
+      "flexbox-stage"
+    ).innerHTML = `<option value="Completed">Level Completed</option>`;
+
+    // calling the function image level function
+
     imageLevel(levels);
   } else {
     const levels = [
       1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-      22, 23, 24, 25, 26, 27, 28,
+      22, 23, 24, 25, 26, 27, 28
     ];
     pageDiv.innerHTML = `<div class="flex-box-froggy">
           <h1>Grid Garden</h1>
@@ -52,6 +72,13 @@ const page = () => {
       </div>
     <div id="flexbox-stage-img"></div>
   </div>`;
+  document.getElementById(
+    "flexbox-stage"
+  ).innerHTML = `<option value="Completed">Level Completed</option>`;
+
+  // calling the function image level function
+
+  imageLevel(levels);
   }
 };
 page();
